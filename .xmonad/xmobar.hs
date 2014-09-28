57a85eb47a2a7a254c66379c2a49d0afb5d7ea84
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-09-28 17:21:06 +1000

    Added xmonad config.

---
+++ .xmonad/xmobar.hs
@@ -0,0 +1,18 @@
+Config { font = "-*-Fixed-R-Normal-*-16-*-*-*-*-*-*-*"
+        , borderColor = "black"
+        , border = TopB
+        , bgColor = "black"
+        , fgColor = "grey"
+        , position = Top
+        , commands = [ Run Weather "YSSY" ["-t","<tempC>C","-L","18","-H","25","--normal","green","--high","red","--low","lightblue"] 36000
+                     , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
+                     , Run Memory ["-t","Mem: <usedratio>%"] 10
+                     , Run Swap [] 10
+                     , Run Com "~/.xmonad/getVolume" [] "getVolume" 10
+                     , Run Date "%_d/%m/%Y %H:%M:%S" "date" 10
+                     , Run StdinReader
+                     ]
+        , sepChar = "%"
+        , alignSep = "}{"
+        , template = "%StdinReader% | %cpu% | %memory%  %swap% | %getVolume% }{<fc=#ee9a00>%date%</fc> | %YSSY% "
+        }
