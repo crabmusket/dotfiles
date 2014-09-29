fda4c9ba03b03e5da671c49a4cc0cade15479331
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-09-29 19:12:08 +1000

    Wireless status in xmobar.

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -8,11 +8,12 @@ Config { font = "-*-Fixed-R-Normal-*-16-*-*-*-*-*-*-*"
                      , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
                      , Run Memory ["-t","Mem: <usedratio>%"] 10
                      , Run Swap [] 10
-                     , Run Com "~/.xmonad/getVolume" [] "getVolume" 10
+                     , Run Com "~/.xmonad/volume.sh" [] "volume" 10
+                     , Run Com "~/.xmonad/wireless.sh" [] "wireless" 50
                      , Run Date "%_d/%m/%Y %H:%M:%S" "date" 10
                      , Run StdinReader
                      ]
         , sepChar = "%"
         , alignSep = "}{"
-        , template = "%StdinReader% | %cpu% | %memory%  %swap% | %getVolume% }{<fc=#ee9a00>%date%</fc> | %YSSY% "
+        , template = "%StdinReader% | %cpu% | %memory%  %swap% | %volume% }{ <fc=#ee9a00>%date%</fc> | %wireless% | %YSSY% "
         }
