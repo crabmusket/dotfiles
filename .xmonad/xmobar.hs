0f552934bb3029555c0926ce647843e2da3e8c94
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-11-30 23:41:25 +1100

    Fixed run commands.

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -8,8 +8,8 @@ Config { font = "-*-Fixed-R-Normal-*-16-*-*-*-*-*-*-*"
                      , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
                      , Run Memory ["-t","Mem: <usedratio>%"] 10
                      , Run Swap [] 10
-                     , Run Com "~/.xmonad/volume.sh" [] "volume" 10
-                     , Run Com "~/.xmonad/wireless.sh" [] "wireless" 50
+                     , Run Com "bash" ["-c", "~/.xmonad/volume.sh"] "volume" 10
+                     , Run Com "bash" ["-c", "~/.xmonad/wireless.sh"] "wireless" 50
                      , Run Date "%_d/%m/%Y %H:%M:%S" "date" 10
                      , Run StdinReader
                      ]
