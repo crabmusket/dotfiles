9dff40796b3ce6914d9a291f5946c0e3c5603fe3
Daniel Buckmaster <dan.buckmaster@gmail.com>
2015-04-09 09:56:49 +1000

    Indentation.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -52,8 +52,8 @@ myLayout = avoidStruts (
     Mirror (Tall 1 (3/100) (1/2)) |||
     tabbed shrinkText tabConfig |||
     Full |||
-    spiral (6/7)) |||
-    noBorders (fullscreenFull Full)
+    spiral (6/7)
+ ) ||| noBorders (fullscreenFull Full)
 
 
 ------------------------------------------------------------------------
